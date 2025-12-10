e88c7507c5e6057d7101b54e7fb66adeacdff0fd
EddyChan1129 <22026266d@connect.polyu.hk>
2025-12-09 22:38:07 -0500

    maximum-subarray

---
+++ 9/12/maximum-subarray.js
@@ -0,0 +1,12 @@
+
+var maxSubArray = function(nums) {
+    let currentSum = nums[0];  
+    let maxSum = nums[0];      
+
+    for (let i = 1; i < nums.length; i++) {
+        currentSum = Math.max(nums[i], nums[i] + currentSum);
+        maxSum = Math.max(maxSum, currentSum);
+    }
+
+    return maxSum;
+};
