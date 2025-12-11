552c6775c339aa5ddff42c1d0510605d387cef05
EddyChan1129 <22026266d@connect.polyu.hk>
2025-12-10 21:38:12 -0500

    better version of lengthOfLongestSubstring

--- 10/12/longest-substring-without-repeating-characters.js
+++ 10/12/longest-substring-without-repeating-characters.js
@@ -19,3 +19,26 @@ var lengthOfLongestSubstring = function(s) {
 
     return biggestNumber;
 };
+
+
+// better vsersion without using array
+var lengthOfLongestSubstring = function(s) {
+    const set = new Set();  // 存住當前 window 入面嘅字
+    let left = 0;           // window 左邊 index
+    let maxLen = 0;         // 最長長度
+
+    for (let right = 0; right < s.length; right++) {
+        const ch = s[right];
+
+        // 如果 set 已經有呢個字，就收窄左邊個 window
+        while (set.has(ch)) {
+            set.delete(s[left]);
+            left++;
+        }
+
+        set.add(ch);  // 加入新字
+        maxLen = Math.max(maxLen, right - left + 1);
+    }
+
+    return maxLen;
+};
